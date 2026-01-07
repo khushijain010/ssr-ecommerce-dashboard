be8fa027d0a00913b976d970d20d1f2f50c77a74
Khushi Jain <khushijain010705@gmail.com>
2026-01-07 22:51:55 +0530

    Fix product update images using nested create

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -1,15 +1,15 @@
-import { auth } from '@clerk/nextjs'
-import { NextResponse } from 'next/server'
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-import prismadb from '@/lib/prismadb'
+import prismadb from "@/lib/prismadb";
 
 export async function GET(
   req: Request,
-  { params }: { params: { productId: string } },
+  { params }: { params: { productId: string } }
 ) {
   try {
     if (!params.productId) {
-      return new NextResponse('Missing productId', { status: 400 })
+      return new NextResponse("Missing productId", { status: 400 });
     }
 
     const product = await prismadb.product.findUnique({
@@ -22,28 +22,28 @@ export async function GET(
         size: true,
         color: true,
       },
-    })
+    });
 
     return new NextResponse(JSON.stringify(product), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
   } catch (error) {
-    console.log('[PRODUCT_GET]', error)
+    console.log("[PRODUCT_GET]", error);
 
-    return new NextResponse('Internal Server Error', { status: 500 })
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { storeId: string; productId: string } },
+  { params }: { params: { storeId: string; productId: string } }
 ) {
   try {
-    const { userId } = auth()
-    const body = await req.json()
+    const { userId } = auth();
+    const body = await req.json();
     const {
       name,
       price,
@@ -53,38 +53,38 @@ export async function PATCH(
       images,
       isFeatured,
       isArchived,
-    } = body
+    } = body;
 
     if (!userId) {
-      return new NextResponse('Unauthorized', { status: 401 })
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!name) {
-      return new NextResponse('Missing name', { status: 400 })
+      return new NextResponse("Missing name", { status: 400 });
     }
 
     if (!price) {
-      return new NextResponse('Missing price', { status: 400 })
+      return new NextResponse("Missing price", { status: 400 });
     }
 
     if (!categoryId) {
-      return new NextResponse('Missing categoryId', { status: 400 })
+      return new NextResponse("Missing categoryId", { status: 400 });
     }
 
     if (!sizeId) {
-      return new NextResponse('Missing sizeId', { status: 400 })
+      return new NextResponse("Missing sizeId", { status: 400 });
     }
 
     if (!colorId) {
-      return new NextResponse('Missing colorId', { status: 400 })
+      return new NextResponse("Missing colorId", { status: 400 });
     }
 
     if (!images || !images.length) {
-      return new NextResponse('Missing images', { status: 400 })
+      return new NextResponse("Missing images", { status: 400 });
     }
 
     if (!params.productId) {
-      return new NextResponse('Missing productId', { status: 400 })
+      return new NextResponse("Missing productId", { status: 400 });
     }
 
     const storeByUserId = await prismadb.store.findFirst({
@@ -92,10 +92,10 @@ export async function PATCH(
         id: params.storeId,
         userId,
       },
-    })
+    });
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 401 })
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     await prismadb.product.update({
@@ -114,43 +114,47 @@ export async function PATCH(
           deleteMany: {},
         },
       },
-    })
+    });
 
     const product = await prismadb.product.update({
       where: {
         id: params.productId,
       },
       data: {
-        
+        images: {
+          create: images.map((image: { url: string }) => ({
+            url: image.url,
+          })),
+        },
       },
-    })
+    });
 
     return new NextResponse(JSON.stringify(product), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
   } catch (error) {
-    console.log('[PRODUCT_PATCH]', error)
+    console.log("[PRODUCT_PATCH]", error);
 
-    return new NextResponse('Internal Server Error', { status: 500 })
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { storeId: string; productId: string } },
+  { params }: { params: { storeId: string; productId: string } }
 ) {
   try {
-    const { userId } = auth()
+    const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse('Unauthorized', { status: 401 })
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!params.productId) {
-      return new NextResponse('Missing productId', { status: 400 })
+      return new NextResponse("Missing productId", { status: 400 });
     }
 
     const storeByUserId = await prismadb.store.findFirst({
@@ -158,27 +162,27 @@ export async function DELETE(
         id: params.storeId,
         userId,
       },
-    })
+    });
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 401 })
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const product = await prismadb.product.deleteMany({
       where: {
         id: params.productId,
       },
-    })
+    });
 
     return new NextResponse(JSON.stringify(product), {
       status: 200,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
   } catch (error) {
-    console.log('[PRODUCT_DELETE]', error)
+    console.log("[PRODUCT_DELETE]", error);
 
-    return new NextResponse('Internal Server Error', { status: 500 })
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
