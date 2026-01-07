95648d1ee873a87c466ec8d06d30b0bd5e8b6786
Khushi Jain <khushijain010705@gmail.com>
2026-01-08 03:15:21 +0530

    fix: revert prisma to default export to stabilize build

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,15 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prismadb = new PrismaClient();
 
-export const prismadb =
-  global.prisma ||
-  new PrismaClient({
-    log: ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prismadb;
-}
+export default prismadb;
