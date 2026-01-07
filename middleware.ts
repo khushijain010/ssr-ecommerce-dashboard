0abb7d182df3331c93976e72ec2240a3510cb004
Khushi Jain <khushijain010705@gmail.com>
2026-01-08 01:23:56 +0530

    force: disable middleware and reset edge cache

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export function middleware() {
+export function middleware(req: NextRequest) {
+  console.log("MIDDLEWARE HIT", req.nextUrl.pathname);
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: "/:path*",
+};
